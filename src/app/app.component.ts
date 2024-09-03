87acd147897a6cf850946b3920bac75f0c684e74
Gustavo Teles <ustavoteles@gmail.com>
2024-09-03 20:48:32 -0300

    Add MongoDB integration and establish communication between Angular frontend and Node.js backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Tarefa } from './tarefa';
 import { ItemComponent } from './item/item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ItemComponent],
+  imports: [CommonModule, ItemComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,25 +18,45 @@ export class AppComponent {
   title = 'TODOapp';
 
   arrayDeTarefas: Tarefa[] = [];
+  apiURL: string;
 
-  constructor() {
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000'
     this.READ_tarefas();
   }
 
   CREATE_tarefa(descricaoNovaTarefa: string) {
     var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
-    this.arrayDeTarefas.unshift(novaTarefa);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
   }
+
 
   READ_tarefas() {
-    this.arrayDeTarefas = [
-      new Tarefa('Estudar Frameworks Web', false),
-      new Tarefa('Comer Pizza', false),
-      new Tarefa('Ajudar meus pais', false),
-    ];
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.arrayDeTarefas = resultado);
+
   }
 
-  DELETE_tarefa (tarefaAserRemovida : Tarefa){
-    this.arrayDeTarefas.splice(this.arrayDeTarefas.indexOf(tarefaAserRemovida),1);
-    }
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada)
+    var id = this.arrayDeTarefas[indice]._id
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+      tarefaAserModificada).subscribe(
+        resultado => {
+          console.log(resultado)
+          this.READ_tarefas()
+        })
+  }
+
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida)
+    var id = this.arrayDeTarefas[indice]._id
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => {
+        console.log(resultado)
+        this.READ_tarefas()
+      })
+  }
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from '../tarefa';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css',
+})
+export class ItemComponent {
+  emEdicao = false;
+  @Input() tarefa: Tarefa = new Tarefa('', false);
+  @Output() removeTarefa = new EventEmitter();
+  @Output() modificaTarefa = new EventEmitter();
+}
