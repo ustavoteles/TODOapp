a3e2e23536292bd7edf79f70249de6cea106ed77
Gustavo Teles <ustavoteles@gmail.com>
2024-09-03 20:47:07 -0300

    Add To Do List using Angular, NodeJS, MongoDB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Tarefa } from './tarefa';
+import { ItemComponent } from './item/item.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  constructor() {
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.arrayDeTarefas.unshift(novaTarefa);
+  }
+
+  READ_tarefas() {
+    this.arrayDeTarefas = [
+      new Tarefa('Estudar Frameworks Web', false),
+      new Tarefa('Comer Pizza', false),
+      new Tarefa('Ajudar meus pais', false),
+    ];
+  }
+
+  DELETE_tarefa (tarefaAserRemovida : Tarefa){
+    this.arrayDeTarefas.splice(this.arrayDeTarefas.indexOf(tarefaAserRemovida),1);
+    }
 }
